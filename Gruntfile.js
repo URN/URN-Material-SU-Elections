dfc43a8b5215e6c9b87e53ce071e97a8bcd1fd82
James Turner <me@jamesturner.im>
2016-02-28 17:34:30 +0000

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        scsslint: {
+            all: [
+                'sass/_partials/*.scss'
+            ]
+        },
+
+        compass: {
+            dist: {
+                options: {
+                    config: 'config.rb'
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            css: {
+                files: ['sass/**/*.scss'],
+                tasks: ['compass'],
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-scss-lint');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-shell');
+
+    grunt.registerTask('default', ['scsslint', 'compass', 'watch']);
+
+};
